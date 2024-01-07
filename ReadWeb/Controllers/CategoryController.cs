c1a1cca79df6d9f8aea0823e4f635c09918169ec
Rohith <rohith.munna28@gmail.com>
2024-01-07 04:35:25 -0800

    First commit till Category

---
+++ ReadWeb/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReadWeb.Data;
+using ReadWeb.Models;
+
+namespace ReadWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList=_db.Categories.ToList();
+            return View(objCategoryList);
+        }
+
+        public IActionResult Create() 
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Category category) 
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
